cc28dab0c14da37e1726ac89545903925c1c239c
MahfoudhiFatma <mahfoudhifatma87@gmail.com>
2018-08-20 20:43:57 +0100

    static-timer

---
+++ src/components/Timer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Timer = () =>{
+    return (
+        <div className="timer-content">
+            <div className="timer-div">
+                <h1>00:</h1>
+                <p>Hour</p>
+            </div>
+            <div className="timer-div">
+                <h1>39:</h1>
+                <p>Minute</p>
+            </div>
+            <div className="timer-div">
+                <h1>00</h1>
+                <p>Second</p>
+            </div>
+</div>
+    )
+}
+
+export default Timer
